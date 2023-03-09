45c1dcf05c6aed8abcc391b57edccac88a8ba3ac
Gonzalo Vásquez <gevs@amazon.com>
2023-03-09 10:46:03 -0300

    syncing local dev environment

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Pack Lambda Functions
+arr_variable=("GetInstances" "GetInstanceTypes" "GetInstanceUtilization" "GetUsage" "StoreRegionalUtilization")
+
+## now loop through the above array
+for lambda in "${arr_variable[@]}"
+do
+    cd "lambdas/$lambda"; npm ci; zip ../$lambda.zip *; cd ../..
+done
+
+
+#Validate Stack
+
+aws cloudformation validate-template --template-body file://template.yaml
+#Deploy Stack
+sam build
+sam deploy --stack-name Utilization --resolve-s3 --capabilities CAPABILITY_NAMED_IAM --region us-west-2
+
+# cleanup
+for lambda in "${arr_variable[@]}"
+do
+    rm lambdas/$lambda.zip
+done
---
+++ lambdas/GetInstances/index.mjs
@@ -0,0 +1,28 @@
+import {
+    EC2Client,
+    DescribeInstancesCommand
+} from "@aws-sdk/client-ec2";
+
+async function getInstances(region) {
+    const instances = []
+    const client = new EC2Client({
+        region
+    });
+    var params = {}
+    const command = new DescribeInstancesCommand(params);
+    var res
+    res = await client.send(command);
+    console.log('res: ', res)
+    for (const reserve of res.Reservations) {
+        for (const instance of reserve.Instances) {
+            console.log(instance.InstanceId, instance.InstanceType)
+            instances.push({id: instance.InstanceId, type: instance.InstanceType})            
+        }
+    }
+    return instances
+}
+export const handler = async(event) => {
+    console.log(event)
+    const instances = await getInstances(event.region)
+    return { region: event.region, instanceTypes: event.instancesTypes, instances };
+};
